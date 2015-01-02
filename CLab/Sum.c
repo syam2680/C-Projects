051aaafcd10c8e4297f6ce4c0d164ee35c641c96
Syam Kumar <syamk@amazon.com>
2015-01-02 09:37:13 +0530

    Sum of series

---
+++ CLab/Sum.c
@@ -0,0 +1,27 @@
+/* 
+/ Sum.c: Syam Kumar M
+/ Description: C program to calculate the Sum -> 1-x2/2!+x4/4!-x6/6!+x8/8!-x10/10!
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+double factorial(int num){
+    double factorial=1;
+    while(num>0){
+    	factorial*=num--;
+    }
+	return factorial;
+}
+
+int main(){
+	int x=10;
+	double sum=1;
+    int exponent=0;
+    for(int i=1;i<=5;i++){
+    	exponent+=2;
+    	sum+=(pow(x,exponent)*pow(-1,i))/factorial(exponent);
+    }
+    printf("%f \n",sum);
+	return 0;
+}
